f7f7245424df44c02e106091c3a0921df5fb454d
Christoph Helma <helma@in-silico.ch>
2015-12-11 10:20:44 +0100

    nfs backup

--- backup.sh
+++ backup.sh
@@ -1,13 +1,16 @@
 #!/bin/sh
-
+#https://wiki.archlinux.org/index.php/Full_system_backup_with_rsync
 # general
-rsync -av --delete --delete-excluded --inplace --exclude-from=$HOME/.backup_exclude /home/ch /backup/`hostname`/home/
+#rsync -av --delete --delete-excluded --inplace --exclude-from=$HOME/.backup_exclude /home/ch /backup/`hostname`/home/
+#sudo rsync -aAXv --delete --delete-excluded --exclude-from=$HOME/.backup_exclude /home /etc /media/nfs/mirror/`hostname`/
+sudo rsync -aAHSv --delete --delete-excluded --exclude-from=$HOME/.backup_exclude /* /media/nfs/mirror/`hostname`/
+#sudo rsync -aAXHSv --delete --delete-excluded --exclude-from=$HOME/.backup_exclude /* /media/nfs/mirror/`hostname`/
 # archive
 #rsync -av --inplace /home/ch/archive /backup/`hostname`/home/ch/
 #rm -rf /home/ch/archive
 #mkdir /home/ch/archive
 # snapshot
-sudo zfs snapshot "backup/zx81@`date +"%Y-%m-%d %T"`"
+#sudo zfs snapshot "backup/zx81@`date +"%Y-%m-%d %T"`"
 # cleanup
 #/home/ch/src/backup/cleanup.rb
 # backup mail
