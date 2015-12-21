b849dbb221f644c662ad341f89f795fea6094b8b
Christoph Helma <helma@in-silico.ch>
2015-12-21 12:43:39 +0100

    rsync mirroring to same disk excluded

--- backup.sh
+++ backup.sh
@@ -6,18 +6,18 @@ DATE=`date +%Y-%m-%dT%H:%M:%S`
 export BORG_PASSPHRASE=`cat $SCRIPTPATH/.pwd`
 START=$(date +%s.%N)
 if mountpoint -q /media/nfs; then
-  rsync -aHS --delete --delete-excluded --exclude-from=$SCRIPTPATH/exclude /* /media/nfs/mirror/`hostname`/
+  #rsync -aHS --delete --delete-excluded --exclude-from=$SCRIPTPATH/exclude /* /media/nfs/mirror/`hostname`/
+  #END=$(date +%s.%N)
+  #DIFF=$(echo "$END - $START" | bc)
+  #echo "rsync: $DIFF"
+  #START=$(date +%s.%N)
+  borg create --exclude-caches --compression zlib /media/nfs/backup::`hostname`-$DATE /
   END=$(date +%s.%N)
   DIFF=$(echo "$END - $START" | bc)
-  echo "rsync: $DIFF"
-  START=$(date +%s.%N)
+  echo "borg: $DIFF"
   if mountpoint -q /media/storagebox; then
-    borg create --exclude-caches --compression zlib /media/nfs/backup::`hostname`-$DATE /media/nfs/mirror/`hostname`
-    END=$(date +%s.%N)
-    DIFF=$(echo "$END - $START" | bc)
-    echo "borg: $DIFF"
     START=$(date +%s.%N)
-    rsync -rltzu --delete /media/nfs/backup/* /media/storagebox/
+    rsync -rltzu --delete /media/nfs/backup/ /media/storagebox/
     END=$(date +%s.%N)
     DIFF=$(echo "$END - $START" | bc)
     echo "rsync: $DIFF"
