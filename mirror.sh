bfd259d512cacd5c38b91eaeb4aa5b138cb7d1c9
Christoph Helma <helma@in-silico.ch>
2016-01-05 12:43:41 +0100

    mirror.sh adjusted for naspi storage

--- mirror.sh
+++ mirror.sh
@@ -3,13 +3,13 @@
 SCRIPT=$(readlink -f "$0")
 SCRIPTPATH=$(dirname "$SCRIPT")
 
-if [ -n "$(ping -c1 -W1 192.168.1.1)" ]; then # router available
+if [ -n "$(ping -c1 -W1 naspi)" ]; then # nas available
   if [ "$#" -eq 0 ]; then # full backup
     #https://wiki.archlinux.org/index.php/Full_system_backup_with_rsync
-    sudo rsync -aHSv --delete --delete-excluded --exclude-from=$SCRIPTPATH/exclude /* admin@192.168.1.1:/mnt/sda1/mirror/`hostname`/
-  elif [[ ( -e "$1" ) && ( -n "$(ls -A $1)" ) && ( "$1" =~ ^/media/ ) ]]; then # directory exists and is not empty
+    sudo rsync -aHSv --delete --delete-excluded --exclude-from=$SCRIPTPATH/exclude /* root@naspi:/mnt/sda1/mirror/`hostname`/
+  elif [[ ( -e "$1" ) && ( -n "$(ls -A $1)" ) && ( "$1" =~ ^/media/ ) ]]; then # /media/* directory exists and is not empty
     # TODO add exclude for pis
-    sudo ssh admin@192.168.1.1 "mkdir -p /mnt/sda1/mirror/`basename "$1"`/"
-    sudo rsync -aHSv --delete $1/* admin@192.168.1.1:/mnt/sda1/mirror/`basename "$1"`/
+    sudo ssh root@naspi "mkdir -p /mnt/sda1/mirror/`basename "$1"`/"
+    sudo rsync -aHSv --delete $1/* root@naspi:/mnt/sda1/mirror/`basename "$1"`/
   fi
 fi
