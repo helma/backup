dbcb5d4a20c741679c672049e5d9909d2256d86f
Christoph Helma <helma@in-silico.ch>
2016-01-06 08:48:41 +0000

    offline-backup.[service|timer] added

--- borg-backup.sh
+++ borg-backup.sh
@@ -6,7 +6,7 @@ DATE=`date +%Y-%m-%dT%H:%M:%S`
 export BORG_PASSPHRASE=`cat $SCRIPTPATH/.pwd`
 START=$(date +%s.%N)
 if mountpoint -q /mnt/sda1; then
-  borg create -v --stats --exclude-caches --compression zlib /mnt/sda1/backup::mpc-$DATE /mnt/sda1/mirror/mpc
+  borg create -v --stats --exclude-caches --compression zlib /mnt/sda1/backup::zx81-$DATE /mnt/sda1/mirror/zx81
   END=$(date +%s.%N)
   DIFF=$(echo "$END - $START" | bc)
   echo "borg: $DIFF"
