f2a6d3b8e641d1287de14a37bfee7600f9c49be0
Christoph Helma <helma@in-silico.ch>
2016-04-21 08:33:57 +0200

    unattended borg prune

--- prune.rb
+++ prune.rb
@@ -1,12 +1,12 @@
 #!/usr/bin/env ruby
-require 'json'
-require 'yaml'
 require 'date'
 
-hosts = `borg list /mnt/sda1/backup`.split("\n").collect do |l|
+pwd = File.read(File.join(File.dirname(__FILE__),".pwd"))
+
+hosts = `export BORG_PASSPHRASE=#{pwd}; borg list /mnt/sda1/backup`.split("\n").collect do |l|
   name,date = l.chomp.split(/\s+/,2)
   name.sub(/-2.*/,'')+"-20"
 end
 hosts.uniq.each do |host|
-  `borg prune -v --keep-within=2d --keep-daily=7 --keep-weekly=4 --keep-monthly=-1 --prefix #{host} /mnt/sda1/backup`
+  `export BORG_PASSPHRASE=#{pwd}; borg prune -v --keep-within=2d --keep-daily=7 --keep-weekly=4 --keep-monthly=-1 --prefix #{host} /mnt/sda1/backup >> /tmp/borg-prune.log 2>&1`
 end
