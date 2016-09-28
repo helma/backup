85df34e0812dc1bd23581ecc530285c0796c1e98
Christoph Helma <helma@in-silico.ch>
2016-09-28 16:21:30 +0200

    backup to usb disk

--- backup.sh
+++ backup.sh
@@ -1,35 +1,25 @@
-#!/bin/bash
+#!/bin/sh
+
+# mount
+mkdir -p /mnt/backup
+mkdir -p /mnt/storagebox
+mount -U 1632b2b3-bfbb-48a9-b417-05bcdbb41e63 /mnt/backup
+sshfs u116373@u116373.your-storagebox.de: /mnt/storagebox
 
 SCRIPT=$(readlink -f "$0")
 SCRIPTPATH=$(dirname "$SCRIPT")
+BACKUP=/mnt/backup/backup
+NAME=$(hostname)
+DATE=`date +%Y-%m-%dT%H:%M:%S`
+export BORG_PASSPHRASE=`cat $SCRIPTPATH/.pwd`
 
-SERVER=root@192.168.1.16
+# backup
+borg create --debug --progress --stats --exclude-caches --compression zlib  --exclude-from $SCRIPTPATH/exclude $BACKUP::$NAME-$DATE /* 
 
-MIRROR=/mnt/sda1/mirror
-BACKUP=/mnt/sda1/backup
+# sync to storagebox
+rsync -rltzuv --delete /mnt/backup/backup/ /mnt/storagebox/
 
-if [ "$#" -eq 0 ]; then # full backup
-  NAME=$(hostname)
-else
-  NAME=$(basename "$1")
-fi
-
-MIRROR_PATH=$SERVER:$MIRROR/$NAME/
-PWD=`cat $SCRIPTPATH/.pwd`
-
-if pidof rsync; then # rsync running
-  echo "rsync is already running"
-else
-  if [ -n "$(ping -c1 -W1 192.168.1.16)" ]; then # nas available
-    ssh root@192.168.1.16 "mkdir -p $MIRROR/$NAME/"
-    if [ "$#" -eq 0 ]; then # full backup
-      #https://wiki.archlinux.org/index.php/Full_system_backup_with_rsync
-      rsync -aHSv --delete --delete-excluded --exclude-from=$SCRIPTPATH/exclude /* $MIRROR_PATH
-    elif [[ ( -e "$1" ) && ( -n "$(ls -A $1)" ) && ( "$1" =~ ^/media/ ) ]]; then # /media/* directory exists and is not empty
-      # TODO add exclude for pis
-      rsync -aHSv --delete $1/* $MIRROR_PATH
-    fi
-    DATE=`date +%Y-%m-%dT%H:%M:%S`
-    ssh root@192.168.1.16 "export BORG_PASSPHRASE=$PWD; nohup borg create --stats --exclude-caches --compression zlib $BACKUP::$NAME-$DATE $MIRROR/$NAME >> /tmp/borg.log 2>&1 &"
-  fi
-fi
+umount /mnt/backup
+rmdir /mnt/backup
+umount /mnt/storagebox
+rmdir /mnt/storagebox
